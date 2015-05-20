13c631d0db1502cbf078c4e973892649cb0f21b3
bugkreator <bugkreator@users.noreply.github.com>
2015-05-21 00:23:03 +0700

    bla

--- run-kafka-all.sh
+++ run-kafka-all.sh
@@ -3,5 +3,5 @@
 sleep 5
 for i in `seq 0 5`; do ./run-kafka-broker.sh $i; done
 #sleep 20
-./run-kafka-consumer.sh topic3
+#./run-kafka-consumer.sh topic5
 
--- run-kafka-consumer.sh
+++ run-kafka-consumer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#gnome-terminal --working-directory=/usr/bin/kafka --title "consumer" -e "bin/kafka-console-consumer.sh --zookeeper localhost:2181 --from-beginning --topic $1" --profile=Default
-gnome-terminal --working-directory=/usr/bin/kafka --title "consumer" -e "bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic $1" --profile=Default
+gnome-terminal --working-directory=/usr/bin/kafka --title "consumer" -e "bin/kafka-console-consumer.sh --zookeeper localhost:2181 --from-beginning --topic $1" --profile=Default
+#gnome-terminal --working-directory=/usr/bin/kafka --title "consumer" -e "bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic $1" --profile=Default
